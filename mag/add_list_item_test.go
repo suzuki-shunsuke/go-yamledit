8458ac3fb1d41dfca3e37b1a61718dc2af3f90aa
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-03-18 02:25:00 +0900

    test: add unit tests

--- mag/add_list_item_test.go
+++ mag/add_list_item_test.go
@@ -3,7 +3,9 @@ package mag_test
 import (
 	"fmt"
 	"log"
+	"testing"
 
+	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
 	"github.com/suzuki-shunsuke/mag-go-sdk/mag"
 )
@@ -37,4 +39,132 @@ children:
 	//   - zoo
 	//   - foo # comment
 	//   - bar
+}
+
+func TestAddListItemAction_Run(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		yml     string
+		action  mag.AddListItemAction
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "add to beginning",
+			yml:  "items:\n- a\n- b\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddListItemEditor("first", 0),
+			},
+			want: "items:\n- first\n- a\n- b\n",
+		},
+		{
+			name: "add to end",
+			yml:  "items:\n- a\n- b\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddListItemEditor("last", 2),
+			},
+			want: "items:\n- a\n- b\n- last\n",
+		},
+		{
+			name: "add to middle",
+			yml:  "items:\n- a\n- b\n- c\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddListItemEditor("mid", 1),
+			},
+			want: "items:\n- a\n- mid\n- b\n- c\n",
+		},
+		{
+			name: "nested path",
+			yml:  "foo:\n  items:\n  - x\n  - y\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.foo.items",
+				Add:      mag.NewStaticAddListItemEditor("z", 0),
+			},
+			want: "foo:\n  items:\n  - z\n  - x\n  - y\n",
+		},
+		{
+			name: "with comment preservation",
+			yml:  "items:\n- a # comment1\n- b # comment2\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddListItemEditor("new", 1),
+			},
+			want: "items:\n- a # comment1\n- new\n- b # comment2\n",
+		},
+		{
+			name: "Add returns Noop",
+			yml:  "items:\n- a\n- b\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add: func(_ *ast.SequenceNode) (any, int, error) {
+					return mag.Noop, 0, nil
+				},
+			},
+			want: "items:\n- a\n- b\n",
+		},
+		{
+			name: "invalid yaml path",
+			yml:  "items:\n- a\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "invalid[",
+				Add:      mag.NewStaticAddListItemEditor("x", 0),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Add is nil",
+			yml:  "items:\n- a\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+			},
+			wantErr: true,
+		},
+		{
+			name: "negative depth",
+			yml:  "items:\n- a\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddListItemEditor("x", 0),
+				Depth:    -1,
+			},
+			wantErr: true,
+		},
+		{
+			name: "depth with sequence of sequences",
+			yml:  "items:\n- - a\n  - b\n- - c\n  - d\n",
+			action: mag.AddListItemAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddListItemEditor("new", 0),
+				Depth:    1,
+			},
+			want: "items:\n- - new\n  - a\n  - b\n- - new\n  - c\n  - d\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			file, err := parser.ParseBytes([]byte(tt.yml), parser.ParseComments)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = tt.action.Run(file.Docs[0].Body)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := file.String()
+			if got != tt.want {
+				t.Errorf("got:\n%s\nwant:\n%s", got, tt.want)
+			}
+		})
+	}
 }
--- mag/add_map_key_test.go
+++ mag/add_map_key_test.go
@@ -3,7 +3,9 @@ package mag_test
 import (
 	"fmt"
 	"log"
+	"testing"
 
+	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
 	"github.com/suzuki-shunsuke/mag-go-sdk/mag"
 )
@@ -37,4 +39,130 @@ name: foo # keep comment
 	// Output:
 	// age: 10
 	// name: foo # keep comment
+}
+
+func TestAddMapKeyAction_Run(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		yml     string
+		action  mag.AddMapKeyAction
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "add to beginning",
+			yml:  "name: foo\nage: 10\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+				Add:      mag.NewStaticAddMapKeyEditor("first", true, 0),
+			},
+			want: "first: true\nname: foo\nage: 10\n",
+		},
+		{
+			name: "add to end",
+			yml:  "name: foo\nage: 10\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+				Add:      mag.NewStaticAddMapKeyEditor("last", "val", 2),
+			},
+			want: "name: foo\nage: 10\nlast: val\n",
+		},
+		{
+			name: "add to middle",
+			yml:  "a: 1\nb: 2\nc: 3\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+				Add:      mag.NewStaticAddMapKeyEditor("mid", "x", 1),
+			},
+			want: "a: 1\nmid: x\nb: 2\nc: 3\n",
+		},
+		{
+			name: "negative index",
+			yml:  "a: 1\nb: 2\nc: 3\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+				Add:      mag.NewStaticAddMapKeyEditor("neg", "x", -1),
+			},
+			want: "a: 1\nb: 2\nneg: x\nc: 3\n",
+		},
+		{
+			name: "nested path",
+			yml:  "foo:\n  bar: 1\n  baz: 2\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$.foo",
+				Add:      mag.NewStaticAddMapKeyEditor("qux", 99, 0),
+			},
+			want: "foo:\nqux: 99\n  bar: 1\n  baz: 2\n",
+		},
+		{
+			name: "with comment on value",
+			yml:  "name: foo\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+				Add:      mag.NewStaticAddMapKeyEditor("color", mag.WithComment("red", "a nice color"), 0),
+			},
+			want: "color: red #a nice color\nname: foo\n",
+		},
+		{
+			name: "sequence of mappings",
+			yml:  "items:\n- name: a\n  val: 1\n- name: b\n  val: 2\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$.items",
+				Add:      mag.NewStaticAddMapKeyEditor("new", true, 0),
+			},
+			want: "items:\n- new: true\n    name: a\n    val: 1\n- new: true\n    name: b\n    val: 2\n",
+		},
+		{
+			name: "Add returns ErrNoop",
+			yml:  "name: foo\nage: 10\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+				Add: func(_ *ast.MappingNode) (any, any, int, error) {
+					return nil, nil, 0, mag.ErrNoop
+				},
+			},
+			want: "name: foo\nage: 10\n",
+		},
+		{
+			name: "invalid yaml path",
+			yml:  "name: foo\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "invalid[",
+				Add:      mag.NewStaticAddMapKeyEditor("x", "y", 0),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Add is nil",
+			yml:  "name: foo\n",
+			action: mag.AddMapKeyAction{
+				YAMLPath: "$",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			file, err := parser.ParseBytes([]byte(tt.yml), parser.ParseComments)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = tt.action.Run(file.Docs[0].Body)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := file.String()
+			if got != tt.want {
+				t.Errorf("got:\n%s\nwant:\n%s", got, tt.want)
+			}
+		})
+	}
 }
