db04c4d715d8878cac198a116288dcfbca6f7a76
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-03-18 02:32:04 +0900

    test: add unit tests

--- mag/remove_list_item_test.go
+++ mag/remove_list_item_test.go
@@ -3,7 +3,9 @@ package mag_test
 import (
 	"fmt"
 	"log"
+	"testing"
 
+	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
 	"github.com/suzuki-shunsuke/mag-go-sdk/mag"
 )
@@ -35,4 +37,189 @@ children:
 	// Output:
 	// children:
 	//   - bar # comment 2
+}
+
+func TestRemoveListItemAction_Run(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		yml     string
+		action  mag.RemoveListItemAction
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "remove first item",
+			yml: `items:
+- a
+- b
+- c
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(0),
+			},
+			want: `items:
+- b
+- c
+`,
+		},
+		{
+			name: "remove last item",
+			yml: `items:
+- a
+- b
+- c
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(2),
+			},
+			want: `items:
+- a
+- b
+`,
+		},
+		{
+			name: "remove middle item",
+			yml: `items:
+- a
+- b
+- c
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(1),
+			},
+			want: `items:
+- a
+- c
+`,
+		},
+		{
+			name: "nested path",
+			yml: `foo:
+  items:
+  - x
+  - y
+  - z
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.foo.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(1),
+			},
+			want: `foo:
+  items:
+  - x
+  - z
+`,
+		},
+		{
+			name: "with comment preservation",
+			yml: `items:
+- a # comment1
+- b # comment2
+- c # comment3
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(1),
+			},
+			want: `items:
+- a # comment1
+- c # comment3
+`,
+		},
+		{
+			name: "Remove returns ErrNoop",
+			yml: `items:
+- a
+- b
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove: func(_ *ast.SequenceNode) (int, error) {
+					return 0, mag.ErrNoop
+				},
+			},
+			want: `items:
+- a
+- b
+`,
+		},
+		{
+			name: "depth with sequence of sequences",
+			yml: `items:
+- - a
+  - b
+- - c
+  - d
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(0),
+				Depth:    1,
+			},
+			want: `items:
+- - b
+- - d
+`,
+		},
+		{
+			name: "invalid yaml path",
+			yml: `items:
+- a
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "invalid[",
+				Remove:   mag.NewStaticRemoveListItemEditor(0),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Remove is nil",
+			yml: `items:
+- a
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+			},
+			wantErr: true,
+		},
+		{
+			name: "negative depth",
+			yml: `items:
+- a
+`,
+			action: mag.RemoveListItemAction{
+				YAMLPath: "$.items",
+				Remove:   mag.NewStaticRemoveListItemEditor(0),
+				Depth:    -1,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			file, err := parser.ParseBytes([]byte(tt.yml), parser.ParseComments)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = tt.action.Run(file.Docs[0].Body)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := file.String()
+			if got != tt.want {
+				t.Errorf("got:\n%s\nwant:\n%s", got, tt.want)
+			}
+		})
+	}
 }
